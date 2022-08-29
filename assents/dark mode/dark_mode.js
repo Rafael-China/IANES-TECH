198b369df278007dfe11301ee40cf9730c4341d1
Rafael Nogueira <102532514+Rafael-China@users.noreply.github.com>
2022-08-29 09:16:07 -0300

    Add files via upload

---
+++ assents/dark mode/dark_mode.js
@@ -0,0 +1,38 @@
+const html = document.querySelector("html")
+const btn = document.querySelector(".button1")
+
+const getStyle = (element, style) => 
+    window
+        .getComputedStyle(element)
+        .getPropertyValue(style)
+
+
+const initialColors = {
+    bg: getStyle(html, "--bg"),
+    bgPanel: getStyle(html, "--bg-panel"),
+    colorHeadings: getStyle(html, "--color-headings"),
+    colorText: getStyle(html, "--color-text"),
+}
+
+const darkMode = {
+    bg: "#333333",
+    bgPanel: "#434343",
+    colorHeadings: "#3664FF",
+    colorText: "#B5B5B5"
+}
+
+const transformKey = key => 
+    "--" + key.replace(/([A-Z])/, "-$1").toLowerCase()
+
+
+const changeColors = (colors) => {
+    Object.keys(colors).map(key => 
+        html.style.setProperty(transformKey(key), colors[key]) 
+    )
+}
+
+
+btn.addEventListener("change", ({target}) => {
+    // target.checked ? changeColors(darkMode) : changeColors(initialColors)
+    changeColors(darkMode)
+})
---
+++ scripts.js
@@ -0,0 +1,174 @@
+/* -------------------------------------------------------------------------------------------------------- */
+/* ---------------------------------------- Criar Conta --------------------------------------------------- */
+/* -------------------------------------------------------------------------------------------------------- */
+
+
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const passwordConfirmation = document.getElementById("password-confirmation");
+
+form.addEventListener("submit", (e) => {
+
+  e.preventDefault();
+
+  checkInputs();
+
+});
+
+function checkInputs() {
+
+  const usernameValue = username.value;
+  const emailValue = email.value;
+  const passwordValue = password.value;
+  const passwordConfirmationValue = passwordConfirmation.value;
+
+  if (usernameValue === "") {
+
+    setErrorFor(username, "O nome de usuário é obrigatório.");
+
+  } else {
+
+    setSuccessFor(username);
+
+  }
+
+  if (emailValue === "") {
+
+    setErrorFor(email, "O email é obrigatório.");
+
+  } else if (!checkEmail(emailValue)) {
+
+    setErrorFor(email, "Por favor, insira um email válido.");
+
+  } else {
+
+    setSuccessFor(email);
+
+  }
+
+  if (passwordValue === "") {
+
+    setErrorFor(password, "A senha é obrigatória.");
+
+  } else if (passwordValue.length < 7) {
+
+    setErrorFor(password, "A senha precisa ter no mínimo 7 caracteres.");
+
+  } else {
+
+    setSuccessFor(password);
+
+  }
+
+  if (passwordConfirmationValue === "") {
+
+    setErrorFor(passwordConfirmation, "A confirmação de senha é obrigatória.");
+
+  } else if (passwordConfirmationValue !== passwordValue) {
+
+    setErrorFor(passwordConfirmation, "As senhas não conferem.");
+
+  } else {
+
+    setSuccessFor(passwordConfirmation);
+
+  }
+
+  const formControls = form.querySelectorAll(".form-control");
+
+  const formIsValid = [...formControls].every((formControl) => {
+
+    return formControl.className === "form-control success";
+
+  });
+
+  if (formIsValid) {
+
+    console.log("O formulário está 100% válido!");
+    criarConta()
+    window.location = "pag_login.html";
+  }
+  
+}
+
+function setErrorFor(input, message) {
+
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+
+  small.innerText = message;
+
+  formControl.className = "form-control error";
+
+}
+
+function setSuccessFor(input) {
+
+  const formControl = input.parentElement;
+
+  formControl.className = "form-control success";
+  
+
+}
+
+function criarConta() {
+
+  var key = Math.floor(Math.random() * 100)
+
+  for (let i = 0; i < localStorage.length; i++) {
+    let id = localStorage.key(i)
+    if (id == key) {
+      criarConta()
+    }
+  }
+
+  var username = document.getElementById("username").value
+  var email = document.getElementById("email").value
+  var password = document.getElementById("password").value
+
+  var usuario = {
+    username_: username,
+    email_: email,
+    password_: password
+  };
+
+  // Armazenar os dados no Local Storage do Navegador
+  localStorage.setItem(key, JSON.stringify(usuario))
+}
+
+function verificaLogin() {
+
+  var email = document.getElementById("email").value
+  var password = document.getElementById("password").value
+
+  for (let i = 0; i < localStorage.length; i++) {
+    let id = localStorage.key(i)
+    let dados = localStorage.getItem(id)
+    let user
+    try {
+      user = JSON.parse(dados)
+    } catch (e) {
+      user = ""
+    }
+
+
+    if (email == user.email_) {
+      if (password != "") {
+        if (password == user.password_) {
+          alert("Bem vindo!!")
+        }
+      }
+      return
+    } 
+  }
+}
+
+function checkEmail(email) {
+
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+  
+}
